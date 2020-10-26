939bc6d339ae3343c3359e9cd18e0bba3dbaa444
Philipy Ruinho <philipyruinho@gmail.com>
2020-10-26 20:00:55 -0300

    trabalho finalizado! Brasil acima de tudo!

---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const String PLAYER1_SYMBOL = 'Bolsomito';
+const Color PLAYER1_COLOR = Colors.grey; // Alfa RGB (0xFFFF0000)
+const String PLAYER1_IMG = 'assets/bolsomito.png';
+
+const String PLAYER2_SYMBOL = 'Lulolatra';
+const Color PLAYER2_COLOR = Colors.grey;
+const String PLAYER2_IMG = 'assets/lulolatra.png';
+
+const String GAME_TITLE = 'Tic Tac Toe';
+const String TIED_TITLE = 'Game Tied!';
+const String WIN_TITLE = '[SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Press the reset button to start again!';
+const String SINGLE_PLAYER_MODE = 'Single Player';
+const String MULTIPLAYER_MODE = 'Two Players';
+const String RESET_BUTTON_LABEL = 'RESET';
